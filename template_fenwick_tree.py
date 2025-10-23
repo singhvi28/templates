db933e94ac3b9c42d15759a2156510c19fd74deb
singhvi28 <akkisinghvi28@gmail.com>
2025-10-23 06:53:05 +0530

    concise fen tree code

--- template_fenwick_tree.py
+++ template_fenwick_tree.py
@@ -33,10 +33,6 @@ class FenwickTree:
             idx -= idx & -idx
         return res
 
-    def range_sum(self, l, r):
-        """Return sum in range [l, r]."""
-        return self.sum(r) - self.sum(l - 1)
-
     def find_kth(self, k):
         """
         Binary lifting: Find smallest index such that prefix_sum(index) >= k.
@@ -52,16 +48,4 @@ class FenwickTree:
             bit_mask >>= 1
         return idx + 1
 
-    def lower_bound(self, target):
-        """
-        Return smallest idx such that prefix_sum(idx) >= target.
-        Same as find_kth(target).
-        """
-        return self.find_kth(target)
-
-    def upper_bound(self, target):
-        """
-        Return smallest idx such that prefix_sum(idx) > target.
-        Equivalent to find_kth(target + 1).
-        """
-        return self.find_kth(target + 1)
+    
