cee0b9221f2421d0c9b5f2eb4a997d200b000217
singhvi28 <akkisinghvi28@gmail.com>
2025-12-08 08:25:40 +0530

    merge sort tree

--- template_segment_tree.py
+++ template_segment_tree.py
@@ -79,3 +79,82 @@ class Update1:
 # seg.make_update(2, 10)
 # # Query again
 # print(seg.make_query(1, 3).val)  # Output: 2^10^4 = 12
+
+from bisect import bisect_right
+
+class MST:
+    def __init__(self, a):
+        self.n = len(a)
+        self.v = [[] for _ in range(4 * self.n)]
+        self.p = [[] for _ in range(4 * self.n)]
+        if self.n > 0:
+            self._build(1, 0, self.n - 1, a)
+
+    def _build(self, idx, l, r, a):
+        if l == r:
+            val = a[l]
+            self.v[idx] = [val]
+            self.p[idx] = [val]
+            return
+        m = (l + r) // 2
+        self._build(idx * 2, l, m, a)
+        self._build(idx * 2 + 1, m + 1, r, a)
+        L = self.v[idx * 2]
+        R = self.v[idx * 2 + 1]
+        merged = []
+        i = j = 0
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                merged.append(L[i])
+                i += 1
+            else:
+                merged.append(R[j])
+                j += 1
+        if i < len(L):
+            merged.extend(L[i:])
+        if j < len(R):
+            merged.extend(R[j:])
+        self.v[idx] = merged
+
+        prefix = []
+        s = 0
+        for val in merged:
+            s += val
+            prefix.append(s)
+        self.p[idx] = prefix
+
+    # internal version
+    def _le(self, idx, l, r, L, R, x):
+        if R < l or r < L:
+            return 0, 0
+        if L <= l and r <= R:
+            A = self.v[idx]
+            P = self.p[idx]
+            pos = bisect_right(A, x)  # first index > x
+            s = P[pos - 1] if pos > 0 else 0
+            return pos, s
+        m = (l + r) // 2
+        a_cnt, a_sum = self._le(idx * 2, l, m, L, R, x)
+        b_cnt, b_sum = self._le(idx * 2 + 1, m + 1, r, L, R, x)
+        return a_cnt + b_cnt, a_sum + b_sum
+
+    # public wrapper: query [L, R], values <= x
+    def le(self, L, R, x):
+        if self.n == 0:
+            return 0, 0
+        return self._le(1, 0, self.n - 1, L, R, x)
+
+    # internal version
+    def _sum(self, idx, l, r, L, R):
+        if R < l or r < L:
+            return 0
+        if L <= l and r <= R:
+            return self.p[idx][-1] if self.p[idx] else 0
+        m = (l + r) // 2
+        return self._sum(idx * 2, l, m, L, R) + self._sum(idx * 2 + 1, m + 1, r, L, R)
+
+    # public wrapper: range sum on [L, R]
+    def range_sum(self, L, R):
+        if self.n == 0:
+            return 0
+        return self._sum(1, 0, self.n - 1, L, R)
